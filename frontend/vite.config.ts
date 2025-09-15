0978059cf7a7819d3a824cc8846f10434fe71d7a
Matheo Dorseuil <m.dorseuil@rt-iut.re>
2025-09-15 11:37:49 +0400

    fix, le docker frontend qui ne se lance pas

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // Important pour Docker
+    port: 3000,
+    watch: {
+      usePolling: true // Pour le hot reload dans Docker
+    }
+  }
 })
